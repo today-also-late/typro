6b7da6622332aef684a0ae1015d83aa55e3fcccd
shuntatakemoto <rt80405@gmail.com>
2021-08-09 18:52:53 +0900

    refactor: storeとsliceをリファクタリングしました

---
+++ redux/slices/userSlice.tsx
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+export type UsersState = {
+  user: string;
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: { uid: "", photoUrl: "", displayName: "", twitterUid: "" },
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = { uid: "", photoUrl: "", displayName: "", twitterUid: "" };
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export const selectUser = (state: RootState) => state.user.user;
+
+export default userSlice.reducer;
--- redux/store.tsx
+++ redux/store.tsx
@@ -1,31 +1,52 @@
-import { Store, combineReducers } from "redux";
-// import logger from "redux-logger";
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import { Store, combineReducers } from "redux";
+// // import logger from "redux-logger";
+// import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+// import answersSlice, {
+//   initialState as answersState,
+// } from "./slices/answerSlice";
+
+// const rootReducer = combineReducers({
+//   answers: answersSlice.reducer,
+// });
+
+// const preloadedState = () => {
+//   return { answers: answersState };
+// };
+
+// export type StoreState = ReturnType<typeof preloadedState>;
+
+// export type ReduxStore = Store<StoreState>;
+
+// const createStore = () => {
+//   // const middlewareList = [...getDefaultMiddleware(), logger];
+
+//   return configureStore({
+//     reducer: rootReducer,
+//     // middleware: middlewareList,
+//     devTools: process.env.NODE_ENV !== "production",
+//     preloadedState: preloadedState(),
+//   });
+// };
+
+// export default createStore;
+
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import answersSlice, {
   initialState as answersState,
 } from "./slices/answerSlice";
+import userReducer from "./slices/userSlice";
 
-const rootReducer = combineReducers({
-  answers: answersSlice.reducer,
+export const store = configureStore({
+  reducer: {
+    answers: answersSlice.reducer,
+    user: userReducer,
+  },
 });
 
-const preloadedState = () => {
-  return { answers: answersState };
-};
-
-export type StoreState = ReturnType<typeof preloadedState>;
-
-export type ReduxStore = Store<StoreState>;
-
-const createStore = () => {
-  // const middlewareList = [...getDefaultMiddleware(), logger];
-
-  return configureStore({
-    reducer: rootReducer,
-    // middleware: middlewareList,
-    devTools: process.env.NODE_ENV !== "production",
-    preloadedState: preloadedState(),
-  });
-};
-
-export default createStore;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
