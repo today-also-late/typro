4b67463652a24a989b82d8942325e18144164d9c
Rai <raimi.rarara@gmail.com>
2021-08-11 22:49:10 +0900

    feat: reloadしたときにAuthを呼び出すように変更

--- src/firebase/Auth.tsx
+++ src/firebase/Auth.tsx
@@ -1,10 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getUser,
-  userState,
-  updateUserState,
-} from "../../redux/slices/user/userSlice";
+import { getUser, updateUserState } from "../../redux/slices/userSlice";
 import { useRouter } from "next/router";
 import { auth, db } from "./firebase";
 import Router from "next/router";
@@ -15,34 +11,40 @@ const Auth = ({ children }: any) => {
   const router = useRouter();
 
   useEffect(() => {
-    // updateStateUser(user);
-    if (router.pathname === "/guests/play") {
+    if (router.pathname === "/users/play") {
       if (!user.isSignedIn /*reduxのStateがfalseならば */) {
-        auth.onAuthStateChanged((user) => {
-          if (user) {
-            const uid = user.uid;
-            db.collection("users")
-              .doc(uid)
-              .get()
-              .then((snapshot) => {
-                const data: any = snapshot.data();
-                dispatch(
-                  updateUserState({
-                    uid: uid,
-                    username: data.username,
-                    isSignedIn: true,
-                    email: data.email,
-                  })
-                );
-              });
-          } else {
-            Router.push("/signin");
-          }
-        });
-        // reduxのstateがリロードされてfalseになったけどfirebaseではログインしていることがあるためlistenAuthStateで調べる
+        Router.push("/signin"); // signinページに飛ばす
       }
     }
   }, [router.pathname]);
+
+  useEffect(() => {
+    if (!user.isSignedIn /*reduxのStateがfalseならば */) {
+      // updateStateUser(user);
+      /* firebaseでログインしているかどうか調べてしていなければsigninに返す関数 */
+      auth.onAuthStateChanged(async (user) => {
+        if (user) {
+          const uid = user.uid;
+          await db
+            .collection("users")
+            .doc(uid)
+            .get()
+            .then((snapshot) => {
+              const data: any = snapshot.data();
+              dispatch(
+                updateUserState({
+                  uid: uid,
+                  username: data.username,
+                  isSignedIn: true,
+                  email: data.email,
+                })
+              );
+            });
+        }
+      });
+    }
+    // reduxのstateがリロードされてfalseになったのをもとに戻す。
+  }, [user]);
 
   return children;
 };
