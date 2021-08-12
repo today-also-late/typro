b4ca10f460ff8c80719816a3e70cee10530f976b
Rai <raimi.rarara@gmail.com>
2021-08-13 01:57:05 +0900

    feat: redux-persistを入れてreload時もstateを維持するようにしました

--- redux/store.tsx
+++ redux/store.tsx
@@ -34,14 +34,30 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import answersSlice from "./slices/answersSlice";
 import questionsSlice from "./slices/questionSlice";
 import userSlice from "./slices/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "./storage";
+import { combineReducers } from "redux";
+import hardSet from "redux-persist/lib/stateReconciler/hardSet";
+import { createStore } from "redux";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-export const store = configureStore({
-  reducer: {
-    answers: answersSlice.reducer,
-    user: userSlice.reducer,
-    questions: questionsSlice.reducer,
-  },
+const rootReducer = combineReducers({
+  answers: answersSlice.reducer,
+  user: userSlice.reducer,
+  questions: questionsSlice.reducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  // whitelist: ["user"], userStateだけpersistにすると、プレイ画面でリロードしたときエラーが発生する。
+  stateReconciler: hardSet,
+};
+const persistedReducer = persistReducer<any, any>(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
