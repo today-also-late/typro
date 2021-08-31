f4eefd80f05e9a1269bc3d4741bc96a1765b7bb8
Rai <raimi.rarara@gmail.com>
2021-08-31 18:47:50 +0900

    feat: ranking機能を追加

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "../../../redux/slices/userSlice";
-import { Label } from "../atoms/index";
+import { Label, PrimaryButton } from "../atoms/index";
 import { AfterLoginButton, BeforeLoginButton } from "../molecules/index";
+import { ClosableDrawer } from "../molecules";
 
 type PROPS = {};
 
 const Header: React.FC<PROPS> = () => {
   const user = useSelector(getUser).user;
+
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerToggle = useCallback(
+    (event: any) => {
+      setOpen(!open);
+    },
+    [setOpen, open]
+  );
 
   return (
     <div className="w-screen fixed flex h-16 bg-gray-400">
@@ -15,10 +25,15 @@ const Header: React.FC<PROPS> = () => {
         <Label labelText="TyPro" href={"/"} />
         <Label labelText="概要" href={"/"} />
         <Label labelText="問題" href={"/users/selectlanguage"} />
-        <Label labelText="ランキング" href={"/"} />
-        <Label labelText="投稿" href={"/"} />
+        <PrimaryButton
+          label={"ランキング"}
+          onClick={(e: any) => handleDrawerToggle(e)}
+        />
+        <Label labelText="投稿" href={"/users/submit"} />
       </div>
-      <div className="w-1/5"></div>
+      <div className="w-1/5">
+        <ClosableDrawer open={open} onClose={handleDrawerToggle} />
+      </div>
       <div className="flex items-center justify-evenly w-2/5">
         {user.isSignedIn ? <AfterLoginButton /> : <BeforeLoginButton />}
       </div>
